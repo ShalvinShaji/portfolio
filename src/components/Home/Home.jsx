9bb84e6bdf0c3036e0f1380c44d799b4dd65b52e
Shalvin shaji <shalvinshaji283@gmail.com>
2024-09-24 02:17:47 +0530

    Navbar menu button changed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,8 @@ const Home = () => {
     <div className="main-container">
       <div>
         <h1>
-          Welcome to the <span className="home">Home</span> Page.
+          The best way to predict the <span className="home"> future</span> is
+          to create it.
         </h1>
         <p>
           This is the main content of the <span className="home">home</span>{" "}
