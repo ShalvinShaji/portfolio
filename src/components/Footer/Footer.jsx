fc380dc2ea18f9c9a8a8a91fe7ba3d0dfeb55a42
Shalvin shaji <shalvinshaji283@gmail.com>
2024-10-01 01:53:23 +0530

    menu button alignment corrected

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className=" container text-center ">
+      <small>
+        &copy; 2024. Designed and Developed by Me with{" "}
+        <span className="highlight-colour">&#10084;</span>
+      </small>
+    </footer>
+  );
+};
+
+export default Footer;
